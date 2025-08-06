139da9b93692f30158ed4db7443a69d680c0a110
AshishProPec <your-email@domain.com>
2025-08-06 14:46:34 +0530

    last update at aug 6/2025

---
+++ Graphs/union.cpp
@@ -0,0 +1,222 @@
+// User function Template for C++
+
+class Solution
+{
+public:
+    static bool cmp(vector<int> &a, vector<int> &b)
+    {
+        return a[2] < b[2];
+    }
+    int findParent_NoPathCompression(vector<int> &parent, int node)
+    {
+        if (parent[node] == node)
+            return node;
+
+        return findParent_NoPathCompression(parent, parent[node]); // no path compression
+    }
+
+    int kruskalsMST(int V, vector<vector<int>> &edges)
+    {
+        sort(begin(edges), end(edges), cmp);
+
+        vector<int> parent(V, 0);
+        for (int i = 0; i < V; i++)
+        {
+            parent[i] = i;
+        }
+        int ans = 0;
+        for (auto &x : edges)
+        {
+            int u = x[0];
+            int v = x[1];
+            int parent_u = findParent_NoPathCompression(parent, u);
+            int parent_v = findParent_NoPathCompression(parent, v);
+
+            if (parent_u != parent_v)
+            {
+                parent[parent_v] = parent_u;
+                ans += x[2];
+            }
+        }
+        return ans;
+    }
+}; // here we are not using path compression to optimize the find operation in union-find structure // O(V*E);
+
+//----------------------------
+class Solution
+{
+public:
+    static bool cmp(vector<int> &a, vector<int> &b)
+    {
+        return a[2] < b[2];
+    }
+    int findParent_pathCompression(vector<int> &parent, int node)
+    {
+        if (parent[node] == node)
+            return node;
+
+        return parent[node] = findParent_pathCompression(parent, parent[node]); // path compression
+    }
+
+    int kruskalsMST(int V, vector<vector<int>> &edges)
+    {
+        sort(begin(edges), end(edges), cmp);
+
+        vector<int> parent(V, 0);
+        for (int i = 0; i < V; i++)
+        {
+            parent[i] = i;
+        }
+        int ans = 0;
+        for (auto &x : edges)
+        {
+            int u = x[0];
+            int v = x[1];
+            int parent_u = findParent_pathCompression(parent, u);
+            int parent_v = findParent_pathCompression(parent, v);
+
+            if (parent_u != parent_v)
+            {
+                parent[parent_v] = parent_u;
+                ans += x[2];
+            }
+        }
+        return ans;
+    }
+}; // here we are using path compression to optimize the find operation in union-find structure
+// O(log N)
+
+//---------------------------------------
+
+// here we are using rank concept to find the union of two sets without path compression
+// O(log N)
+// User function Template for C++
+class Solution
+{
+public:
+    static bool cmp(vector<int> &a, vector<int> &b)
+    {
+        return a[2] < b[2];
+    }
+
+    int findParent_noPathCompression(vector<int> &parent, int node)
+    {
+        if (parent[node] == node)
+            return node;
+
+        return findParent_noPathCompression(parent, parent[node]); // without path compression path compression
+    }
+
+    void unionByRank(vector<int> &parent, vector<int> &rank, int parent_u, int parent_v)
+    {
+
+        if (rank[parent_u] < rank[parent_v])
+        {
+            rank[parent_v]++;
+            parent[parent_u] = parent_v;
+        }
+        else if (rank[parent_u] > rank[parent_v])
+        {
+            rank[parent_u]++;
+            parent[parent_v] = parent_u;
+        }
+        else
+        {
+            rank[parent_u]++;
+            parent[parent_v] = parent_u;
+        }
+    }
+
+    int kruskalsMST(int V, vector<vector<int>> &edges)
+    {
+        sort(begin(edges), end(edges), cmp);
+
+        vector<int> parent(V, 0);
+        for (int i = 0; i < V; i++)
+        {
+            parent[i] = i;
+        }
+        vector<int> rank(V, 0);
+        int ans = 0;
+        for (auto &x : edges)
+        {
+            int u = x[0];
+            int v = x[1];
+            int parent_u = findParent_noPathCompression(parent, u);
+            int parent_v = findParent_noPathCompression(parent, v);
+
+            if (parent_u != parent_v)
+            { // to ensure both does not belong to the same component/set
+                unionByRank(parent, rank, parent_u, parent_v);
+                ans += x[2];
+            }
+        }
+        return ans;
+    }
+};
+//------------------------------------------
+// here we are using rank concept to find the union of two sets with path compression
+// O(1)
+// User function Template for C++
+class Solution
+{
+public:
+    static bool cmp(vector<int> &a, vector<int> &b)
+    {
+        return a[2] < b[2];
+    }
+    int findParent_pathCompression(vector<int> &parent, int node)
+    {
+        if (parent[node] == node)
+            return node;
+
+        return parent[node] = findParent_pathCompression(parent, parent[node]); // path compression
+    }
+
+    void unionByRank(vector<int> &parent, vector<int> &rank, int parent_u, int parent_v)
+    {
+
+        if (rank[parent_u] < rank[parent_v])
+        {
+            rank[parent_v]++;
+            parent[parent_u] = parent_v;
+        }
+        else if (rank[parent_u] > rank[parent_v])
+        {
+            rank[parent_u]++;
+            parent[parent_v] = parent_u;
+        }
+        else
+        {
+            rank[parent_u]++;
+            parent[parent_v] = parent_u;
+        }
+    }
+
+    int kruskalsMST(int V, vector<vector<int>> &edges)
+    {
+        sort(begin(edges), end(edges), cmp);
+
+        vector<int> parent(V, 0);
+        for (int i = 0; i < V; i++)
+        {
+            parent[i] = i;
+        }
+        vector<int> rank(V, 0);
+        int ans = 0;
+        for (auto &x : edges)
+        {
+            int u = x[0];
+            int v = x[1];
+            int parent_u = findParent_pathCompression(parent, u);
+            int parent_v = findParent_pathCompression(parent, v);
+
+            if (parent_u != parent_v)
+            { // to ensure both does not belong to the same component/set
+                unionByRank(parent, rank, parent_u, parent_v);
+                ans += x[2];
+            }
+        }
+        return ans;
+    }
+};
