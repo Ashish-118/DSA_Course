b82a4bebbafac51ac5539241f98d27801cdc8d34
AshishProPec <ashishpersonal200@gmail.com>
2026-01-16 18:11:21 +0530

    up

--- greedy/2975MaxSqAreaByRemovingFencesFromAfield.cpp
+++ greedy/2975MaxSqAreaByRemovingFencesFromAfield.cpp
@@ -56,3 +56,61 @@ public:
         return maxSq;
     }
 };
+
+// no need to sort
+
+#define mod 1000000007
+class Solution
+{
+public:
+    int maximizeSquareArea(int m, int n, vector<int> &hFences,
+                           vector<int> &vFences)
+    {
+        unordered_set<int> maxHorizontalLength;
+        unordered_set<int> maxVerticalLength;
+
+        hFences.push_back(1);
+        hFences.push_back(m);
+        vFences.push_back(1);
+        vFences.push_back(n);
+
+        int l1 = hFences.size(), l2 = vFences.size();
+
+        for (int i = 0; i < l1 - 1; i++)
+        {
+            for (int j = i + 1; j < l1; j++)
+            {
+                maxVerticalLength.insert(abs(hFences[j] - hFences[i]));
+            }
+        }
+
+        for (int i = 0; i < l2 - 1; i++)
+        {
+            for (int j = i + 1; j < l2; j++)
+            {
+                maxHorizontalLength.insert(abs(vFences[j] - vFences[i]));
+            }
+        }
+
+        int maxSq = 0;
+
+        for (auto h : maxHorizontalLength)
+        {
+            if (maxVerticalLength.find(h) != maxVerticalLength.end())
+            {
+                maxSq = max(maxSq, h);
+            }
+        }
+
+        if (maxSq == 0)
+        {
+            maxSq = -1;
+        }
+        else
+        {
+            maxSq = 1ll * maxSq * maxSq % mod;
+        }
+
+        return maxSq;
+    }
+};
