bad024aa104176f937aa5978d31b34d3c7983366
AshishProPec <ashishpersonal200@gmail.com>
2026-01-21 07:54:19 +0530

    up

---
+++ bitManipulation/3314ConstructTheMiniBitwiseArrayII.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+    vector<int> minBitwiseArray(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> ans(n, -1);
+
+        for (int i = 0; i < n; i++)
+        {
+
+            int p = -1;
+
+            for (int shift = 0; shift < 32; shift++)
+            {
+                if (nums[i] & (1 << shift))
+                {
+                    p = shift;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (p != -1)
+            {
+                ans[i] = nums[i] - pow(2, p);
+            }
+        }
+
+        return ans;
+    }
+};
