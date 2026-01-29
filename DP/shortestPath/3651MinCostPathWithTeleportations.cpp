dc3e1cf2961d8049a4c513fe97953ed348310c8d
AshishProPec <ashishpersonal200@gmail.com>
2026-01-29 09:47:30 +0530

    update

--- DP/shortestPath/3651MinCostPathWithTeleportations.cpp
+++ DP/shortestPath/3651MinCostPathWithTeleportations.cpp
@@ -77,62 +77,77 @@ class Solution
 public:
     int minCost(vector<vector<int>> &grid, int k)
     {
-        int m = grid.size(), n = grid[0].size();
-        vector<pair<int, int>> points;
-        for (int i = 0; i < m; i++)
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> cost(n, vector<int>(m, 1e9));
+
+        vector<pair<int, int>> cells;
+        for (int i = 0; i < n; i++)
         {
-            for (int j = 0; j < n; j++)
+            for (int j = 0; j < m; j++)
             {
-                points.push_back({i, j});
+                cells.push_back({i, j});
             }
         }
-        sort(points.begin(), points.end(),
-             [&](const auto &p1, const auto &p2) -> bool
+
+        sort(cells.begin(), cells.end(),
+             [&](auto a, auto b)
              {
-                 return grid[p1.first][p1.second] < grid[p2.first][p2.second];
+                 return grid[a.first][a.second] <
+                        grid[b.first][b.second];
              });
-        vector<vector<int>> costs(m, vector<int>(n, INT_MAX));
-        for (int t = 0; t <= k; t++)
+
+        for (int step = 0; step <= k; step++)
         {
-            int minCost = INT_MAX;
-            for (int i = 0, j = 0; i < points.size(); i++)
+
+            int best = 1e9;
+
+            for (int idx = 0; idx < cells.size(); idx++)
             {
-                minCost =
-                    min(minCost, costs[points[i].first][points[i].second]);
-                if (i + 1 < points.size() &&
-                    grid[points[i].first][points[i].second] ==
-                        grid[points[i + 1].first][points[i + 1].second])
+                int r = cells[idx].first;
+                int c = cells[idx].second;
+
+                best = min(best, cost[r][c]);
+
+                if (idx + 1 < cells.size() &&
+                    grid[r][c] ==
+                        grid[cells[idx + 1].first][cells[idx + 1].second])
                 {
                     continue;
                 }
-                for (int r = j; r <= i; r++)
+
+                int back = idx;
+                while (back >= 0 &&
+                       grid[cells[back].first][cells[back].second] == grid[r][c])
                 {
-                    costs[points[r].first][points[r].second] = minCost;
+                    cost[cells[back].first][cells[back].second] = best;
+                    back--;
                 }
-                j = i + 1;
             }
-            for (int i = m - 1; i >= 0; i--)
+
+            for (int i = n - 1; i >= 0; i--)
             {
-                for (int j = n - 1; j >= 0; j--)
+                for (int j = m - 1; j >= 0; j--)
                 {
-                    if (i == m - 1 && j == n - 1)
+
+                    if (i == n - 1 && j == m - 1)
                     {
-                        costs[i][j] = 0;
+                        cost[i][j] = 0;
                         continue;
                     }
-                    if (i != m - 1)
-                    {
-                        costs[i][j] =
-                            min(costs[i][j], costs[i + 1][j] + grid[i + 1][j]);
-                    }
-                    if (j != n - 1)
-                    {
-                        costs[i][j] =
-                            min(costs[i][j], costs[i][j + 1] + grid[i][j + 1]);
-                    }
+
+                    if (i + 1 < n)
+                        cost[i][j] = min(cost[i][j],
+                                         cost[i + 1][j] + grid[i + 1][j]);
+
+                    if (j + 1 < m)
+                        cost[i][j] = min(cost[i][j],
+                                         cost[i][j + 1] + grid[i][j + 1]);
                 }
             }
         }
-        return costs[0][0];
+
+        return cost[0][0];
     }
 };
