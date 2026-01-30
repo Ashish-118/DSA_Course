bb565d5b3e7266b46c3833c7f16e83def2228d92
AshishProPec <ashishpersonal200@gmail.com>
2026-01-30 09:53:02 +0530

    update

---
+++ string/stringMatchMaking/459RepeatedSubstringPattern.cpp
@@ -0,0 +1,41 @@
+// T.C : O(n * root(n))
+/*
+  The "if(n%l == 0)" body will only run for divisors of n.
+  There can be 2*root(n) divisors of a number n.
+  Inside if condition, we call substr which takes n and also run a for loop for
+  n/l times ----- n + n/l ~ O(n)
+
+  So, Time complexity becomes = O(2* root(n) * n)  ~~ O(n * root(n))
+*/
+
+class Solution
+{
+public:
+    bool repeatedSubstringPattern(string s)
+    {
+        int n = s.length();
+
+        // You can also try from l = n/2 to l = 1 -> for(int l = n/2; l >= 0;
+        // l--) //This can be helpful for large test valid cases to return early
+        for (int l = 1; l <= n / 2; l++)
+        {
+
+            if (n % l == 0)
+            {
+                int times = n / l;
+
+                string pattern = s.substr(0, l);
+                string newStr = "";
+                while (times--)
+                {
+                    newStr += pattern;
+                }
+
+                if (newStr == s)
+                    return true;
+            }
+        }
+
+        return false;
+    }
+};
