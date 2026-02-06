a7c0abe8db4e839ff86a2c8485f1a8d45e261692
AshishProPec <ashishpersonal200@gmail.com>
2026-02-06 20:53:48 +0530

    arr

---
+++ Arrays/3634MinRemovalsToBalanceAnArr.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int minRemoval(vector<int> &nums, int k)
+    {
+        sort(begin(nums), end(nums));
+        int n = nums.size();
+        int ans = INT_MAX;
+
+        for (int i = n - 1; i >= 1; i--)
+        {
+            int t = ceil((double)nums[i] / k);
+            int index = lower_bound(begin(nums), end(nums), t) - nums.begin();
+            if (index < i)
+            {
+                ans = min(index + (n - 1 - i), ans);
+            }
+        }
+        return ans == INT_MAX ? n - 1 : ans;
+    }
+};
