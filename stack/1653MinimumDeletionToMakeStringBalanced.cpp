c32d977deda5b0b74cd7ae3bc0c8596ff99d8f27
AshishProPec <ashishpersonal200@gmail.com>
2026-02-07 16:46:57 +0530

    update

--- stack/1653MinimumDeletionToMakeStringBalanced.cpp
+++ stack/1653MinimumDeletionToMakeStringBalanced.cpp
@@ -22,3 +22,30 @@ public:
         return count;
     }
 };
+
+// we can also do with forward pass
+
+class Solution
+{
+public:
+    int minimumDeletions(string s)
+    {
+        stack<char> st;
+
+        int n = s.length();
+        int count = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (!st.empty() && s[i] == 'a' && st.top() == 'b')
+            {
+                st.pop();
+                count++;
+            }
+            else
+                st.push(s[i]);
+        }
+
+        return count;
+    }
+};
