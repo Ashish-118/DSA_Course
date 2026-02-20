5bbc79f2d38306a9c11fc10a1825e7eb0f75ea05
AshishProPec <ashishpersonal200@gmail.com>
2026-02-20 10:03:19 +0530

    update

--- LINKEDLIST/160IntersectionOfTwoLL.cpp
+++ LINKEDLIST/160IntersectionOfTwoLL.cpp
@@ -65,3 +65,28 @@ public:
         return NULL;
     }
 };
+
+/// most optimal approach
+
+class Solution
+{
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+    {
+
+        if (!headA || !headB)
+            return NULL;
+
+        ListNode *tempA = headA;
+        ListNode *tempB = headB;
+
+        while (tempA != tempB)
+        {
+
+            tempA = (tempA == NULL) ? headB : tempA->next;
+            tempB = (tempB == NULL) ? headA : tempB->next;
+        }
+
+        return tempA;
+    }
+};
