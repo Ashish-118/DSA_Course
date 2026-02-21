339d10ce6eb046f8a08734827bad11a09f46ebff
AshishProPec <ashishpersonal200@gmail.com>
2026-02-21 08:56:06 +0530

    update

--- leetDaily/762PrimeNO_ofSetBitsInBinaryRepres.cpp
+++ leetDaily/762PrimeNO_ofSetBitsInBinaryRepres.cpp
@@ -26,3 +26,21 @@ public:
         return ans;
     }
 };
+
+// another approach
+
+int countPrimeSetBits(int left, int right)
+{
+    unordered_set<int> primes = {2, 3, 5, 7, 11, 13, 17, 19};
+    int count = 0;
+
+    for (int num = left; num <= right; num++)
+    {
+        if (primes.count(__builtin_popcount(num)))
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
