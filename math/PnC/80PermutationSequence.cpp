bf53f33f03e692036622d49c858e663a2a38f06b
AshishProPec <ashishpersonal200@gmail.com>
2026-02-22 17:21:19 +0530

    update

---
+++ math/PnC/80PermutationSequence.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+    string getPermutation(int n, int k)
+    {
+        int fact = 1;
+        vector<int> numbers;
+
+        // Build factorial and number list
+        for (int i = 1; i < n; i++)
+        {
+            fact *= i;
+            numbers.push_back(i);
+        }
+        numbers.push_back(n);
+
+        string ans = "";
+        k = k - 1; // convert to 0-based index
+
+        while (true)
+        {
+            ans += to_string(numbers[k / fact]);
+            numbers.erase(numbers.begin() + k / fact);
+
+            if (numbers.size() == 0)
+                break;
+
+            k = k % fact;
+            fact = fact / numbers.size();
+        }
+
+        return ans;
+    }
+};
