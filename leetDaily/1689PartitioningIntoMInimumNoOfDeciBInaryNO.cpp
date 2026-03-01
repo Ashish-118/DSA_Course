6c407ec5a5d898004b9ba22d9ce7e88958d62cff
AshishProPec <ashishpersonal200@gmail.com>
2026-03-01 14:27:56 +0530

    potd

---
+++ leetDaily/1689PartitioningIntoMInimumNoOfDeciBInaryNO.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    int minPartitions(string n)
+    {
+        int ans = 0;
+
+        for (auto x : n)
+        {
+            ans = max(ans, x - '0');
+        }
+
+        return ans;
+    }
+};
