9f0ee34082e2ffe8ecdfdcd1d3db59b970376e1d
AshishProPec <ashishpersonal200@gmail.com>
2026-03-19 13:51:20 +0530

    iupdate

--- DP/2D/877StoneGame.cpp
+++ DP/2D/877StoneGame.cpp
@@ -23,3 +23,33 @@ public:
         return val > sum / 2;
     }
 };
+
+// bottom up
+
+bool stoneGame(vector<int> &piles)
+{
+    int sum = accumulate(begin(piles), end(piles), 0);
+    memset(t, 0, sizeof(t));
+    int n = piles.size();
+
+    // bottom up
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            int commonCase =
+                ((i + 1 <= j - 1 && i + 1 < n && j > 0) ? t[i + 1][j - 1]
+                                                        : 0);
+            int takeStartV1 = (i + 2 < n && i + 2 <= j) ? t[i + 2][j] : 0;
+            int takeEndV1 = (j > 1 && i <= j - 2) ? t[i][j - 2] : 0;
+
+            int takeStart = piles[i] + max(takeStartV1, commonCase);
+
+            int takeEnd = piles[j] + max(takeEndV1, commonCase);
+            t[i][j] = max(takeStart, takeEnd);
+        }
+    }
+
+    return t[0][n - 1] > sum / 2;
+}
