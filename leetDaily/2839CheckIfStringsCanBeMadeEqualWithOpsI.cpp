8cdc8faeea71ffd4b523de61d11f5285a94e38f3
AshishProPec <ashishpersonal200@gmail.com>
2026-03-29 21:08:30 +0530

    update

---
+++ leetDaily/2839CheckIfStringsCanBeMadeEqualWithOpsI.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+    bool canBeEqual(string s1, string s2)
+    {
+
+        if (s1[0] != s2[0])
+        {
+            if (s1[0] == s2[2])
+            {
+                swap(s2[0], s2[2]);
+            }
+            else
+                return false;
+        }
+
+        if (s1[1] != s2[1])
+        {
+            if (s1[1] == s2[3])
+            {
+                swap(s2[1], s2[3]);
+            }
+            else
+                return false;
+        }
+
+        if (s1[2] != s2[2])
+            return false;
+        if (s1[3] != s2[3])
+            return false;
+
+        return true;
+    }
+};
