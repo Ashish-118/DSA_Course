748d26ce848777c712b32141c3989eb498a1e5d8
AshishProPec <ashishpersonal200@gmail.com>
2026-05-04 18:05:07 +0530

    update

--- leetDaily/3225MaxScoreFromGridOps.cpp
+++ leetDaily/3225MaxScoreFromGridOps.cpp
@@ -1,101 +1,67 @@
-// approach 1
+// T.C : O(n^3)
+// S.C : O(n^2)
 class Solution
 {
 public:
-    long long t[100][100][101];
-    vector<vector<long long>> prefixSum;
+    typedef long long ll;
+    int n;
+    ll t[2][101][101];
 
-    long long solve(int row, int col, int prevRow, vector<vector<int>> &grid,
-                    int n)
+    ll solve(bool prevTaken, int prevHeight, int col, vector<vector<int>> &grid, vector<vector<ll>> &colPrefSum)
     {
-        if (col >= n)
+
+        if (col == n)
+        {
             return 0;
-
-        if (t[row][col][prevRow] != -1)
-            return t[row][col][prevRow];
-
-        long long maxi = LLONG_MIN;
-        long long total = 0;
-
-        if (prevRow > 0)
-        {
-            int realPrev = prevRow - 1;
-            total = prefixSum[realPrev][col] -
-                    (col > 0 ? prefixSum[realPrev][col - 1] : 0);
         }
 
-        long long sum = 0;
+        ll result = 0;
 
-        for (int j = row; j < n; j++)
+        if (t[prevTaken][prevHeight][col] != -1)
         {
-            long long usingPrevBlackBox = LLONG_MIN;
-            long long leavingSomeUpperElements = LLONG_MIN;
-            long long usingNxtColBlackBox = LLONG_MIN;
+            return t[prevTaken][prevHeight][col];
+        }
 
-            sum += grid[j][col];
+        for (int height = 0; height <= n; height++)
+        {
+            ll prevColScore = 0;
+            ll currColScore = 0;
 
-            if (prevRow > 0 && (prevRow - 1) >= j)
+            if (!prevTaken && col - 1 >= 0 && height > prevHeight)
             {
-                usingPrevBlackBox = solve(0, col + 1, 0, grid, n); // -1 → 0
-                leavingSomeUpperElements = solve(0, col + 1, j + 1, grid, n);
-
-                if (usingPrevBlackBox != LLONG_MIN)
-                    usingPrevBlackBox += sum;
-
-                if (leavingSomeUpperElements != LLONG_MIN)
-                    leavingSomeUpperElements += (total - sum);
+                prevColScore += colPrefSum[height][col] - colPrefSum[prevHeight][col];
             }
 
-            if (col + 1 < n)
+            if (prevHeight > height)
             {
-                if (j + 1 == n)
-                {
-                    usingNxtColBlackBox = solve(0, col + 2, n, grid, n);
-                }
-                else
-                {
-                    usingNxtColBlackBox = solve(j + 1, col + 1, row, grid, n);
-                }
-
-                if (usingNxtColBlackBox != LLONG_MIN)
-                    usingNxtColBlackBox += sum;
+                currColScore += colPrefSum[prevHeight][col + 1] - colPrefSum[height][col + 1];
             }
 
-            maxi = max(maxi, max({usingPrevBlackBox, usingNxtColBlackBox,
-                                  leavingSomeUpperElements}));
+            ll currColScoreTaken = currColScore + prevColScore + solve(true, height, col + 1, grid, colPrefSum);
+            ll currColScoreNotTaken = prevColScore + solve(false, height, col + 1, grid, colPrefSum);
+
+            result = max({result, currColScoreTaken, currColScoreNotTaken});
         }
 
-        if (row == 0)
-        {
-            long long ret = solve(0, col + 1, n, grid, n);
-            maxi = max(maxi, ret);
-        }
-        else
-        {
-            long long ret = solve(0, col + 1, row, grid, n);
-            maxi = max(maxi, ret);
-        }
-
-        return t[row][col][prevRow] = maxi;
+        return t[prevTaken][prevHeight][col] = result;
     }
 
     long long maximumScore(vector<vector<int>> &grid)
     {
-        int n = grid.size();
-        prefixSum.assign(n, vector<long long>(n, 0));
+        n = grid.size();
 
-        for (int i = 0; i < n; i++)
+        memset(t, -1, sizeof(t));
+
+        vector<vector<ll>> colPrefSum(n + 1, vector<ll>(n + 1, 0));
+
+        for (int col = 1; col <= n; col++)
         {
-            for (int j = 0; j < n; j++)
+            for (int row = 1; row <= n; row++)
             {
-                prefixSum[i][j] =
-                    (i > 0 ? prefixSum[i - 1][j] : 0) +
-                    (j > 0 ? prefixSum[i][j - 1] : 0) -
-                    (i > 0 && j > 0 ? prefixSum[i - 1][j - 1] : 0) + grid[i][j];
+                colPrefSum[row][col] = colPrefSum[row - 1][col] + grid[row - 1][col - 1];
             }
         }
 
-        memset(t, -1, sizeof(t));
-        return solve(0, 0, 0, grid, n);
+        return solve(false, 0, 0, grid, colPrefSum);
     }
 };
