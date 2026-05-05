c448054ceddcc492e8cafed070e15300397a95a4
AshishProPec <ashishpersonal200@gmail.com>
2026-05-05 09:08:21 +0530

    ipdate

--- LINKEDLIST/61RotateList.cpp
+++ LINKEDLIST/61RotateList.cpp
@@ -1,3 +1,5 @@
+// approach 1-- using map
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -46,6 +48,61 @@ public:
             head = last;
             last = prevElement;
         }
+
+        return head;
+    }
+};
+
+/// approach 2-- most optimal
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *rotateRight(ListNode *head, int k)
+    {
+
+        if (head == nullptr)
+            return head;
+
+        int len = 0;
+
+        ListNode *temp = head;
+
+        while (temp->next != nullptr)
+        {
+            len++;
+
+            temp = temp->next;
+        }
+
+        len++;
+
+        ListNode *last = temp;
+
+        k = k % len;
+        if (k == 0)
+            return head;
+        k = len - k - 1;
+        temp = head;
+        while (k--)
+        {
+            temp = temp->next;
+        }
+
+        ListNode *next = temp->next;
+        temp->next = nullptr;
+        last->next = head;
+        head = next;
 
         return head;
     }
