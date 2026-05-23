624666207f183b8833be65e30b84d8ddbc6795a5
AshishProPec <ashishpersonal200@gmail.com>
2026-05-23 09:38:21 +0530

    update

--- Arrays/easy/checkIfArrayIsSortedAndRotated.cpp
+++ Arrays/easy/checkIfArrayIsSortedAndRotated.cpp
@@ -19,3 +19,46 @@ public:
         return count <= 1;
     }
 };
+
+// approach -2
+
+class Solution
+{
+public:
+    bool check(vector<int> &nums)
+    {
+        int n = nums.size();
+
+        int f1 = nums[0];
+        int idx = n;
+        for (int i = 1; i < n; i++)
+        {
+            if (nums[i - 1] > nums[i])
+            {
+                idx = i;
+                break;
+            }
+        }
+
+        if (idx == n)
+            return true;
+
+        int l2 = nums[idx];
+        int idx1 = n;
+        for (int i = idx + 1; i < n; i++)
+        {
+
+            if (nums[i - 1] > nums[i])
+            {
+                idx1 = i;
+                break;
+            }
+            l2 = nums[i];
+        }
+
+        if (idx1 != n || l2 > f1)
+            return false;
+
+        return true;
+    }
+};
