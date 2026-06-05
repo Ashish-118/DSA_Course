156b51db54dbf57c25c5c8a654bf77bc539bc37f
AshishProPec <ashishpersonal200@gmail.com>
2026-06-05 22:28:57 +0530

    potd

--- math/3932CountKthRootsInARange.cpp
+++ math/3932CountKthRootsInARange.cpp
@@ -25,3 +25,36 @@ public:
         return a1-a2;
     }
 };
+
+
+///
+
+class Solution
+{
+public:
+    int countKthRoots(int l, int r, int k)
+    {
+        if (k == 1)
+            return r - l + 1;
+
+        int a1 = 0, a2 = 0;
+
+        for (int i = 0; i <= r; i++)
+        {
+            if (pow(i, k) <= l - 1)
+                a2++;
+
+            if (pow(i, k) <= r)
+                a1++;
+            else
+                break;
+        }
+
+        // for (int i = 0; i <= l - 1; i++) {
+
+        //     else break;
+        // }
+
+        return a1 - a2;
+    }
+};
