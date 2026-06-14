01a926afa319317004d126dcdcc0bb0e167da7cc
AshishProPec <ashishpersonal200@gmail.com>
2026-06-14 17:07:00 +0530

    potd

---
+++ leetDaily/2130MaxTwinSumOfLL.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        ListNode* temp = head;
+        vector<int> v;
+        while (temp != nullptr) {
+
+            v.push_back(temp->val);
+            temp = temp->next;
+        }
+
+        int n=v.size();
+        int ans=-1;
+
+        for(int i=0; i<(n/2); i++){
+           ans=max(ans,v[i]+v[n-1-i]);
+        }
+
+        return ans;
+    }
+};
