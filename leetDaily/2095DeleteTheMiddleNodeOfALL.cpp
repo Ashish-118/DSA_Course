8daf78b8d1610015bc5b32df3805e5448ac1ce70
AshishProPec <ashishpersonal200@gmail.com>
2026-06-15 09:03:19 +0530

    update

---
+++ leetDaily/2095DeleteTheMiddleNodeOfALL.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* prev = nullptr;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next;
+            if (fast != nullptr) {
+                fast = fast->next;
+            }
+        }
+
+        if (prev != nullptr) {
+            prev->next = slow->next;
+            slow->next = nullptr;
+            delete slow;
+
+            return head;
+        }
+        return nullptr;
+    }
+};
