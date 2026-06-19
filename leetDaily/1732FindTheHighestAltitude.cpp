bac308113174d7abf6ec05da46088e56dfc49895
AshishProPec <ashishpersonal200@gmail.com>
2026-06-19 08:27:22 +0530

    potd

---
+++ leetDaily/1732FindTheHighestAltitude.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    int largestAltitude(vector<int> &gain)
+    {
+        int ans = 0;
+        int alt = 0;
+
+        for (auto x : gain)
+        {
+            alt += x;
+            ans = max(alt, ans);
+        }
+
+        return ans;
+    }
+};
