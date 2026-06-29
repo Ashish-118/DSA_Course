696c89de225c354572ad63587ca6a01fa477a39a
AshishProPec <ashishpersonal200@gmail.com>
2026-06-29 08:27:52 +0530

    potd

---
+++ leetDaily/1967NumberOfStringThatAppearAsSubStrInWord.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    int numOfStrings(vector<string> &patterns, string word)
+    {
+        int ans = 0;
+
+        for (auto x : patterns)
+        {
+            if (word.find(x) != string::npos)
+            {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+};
