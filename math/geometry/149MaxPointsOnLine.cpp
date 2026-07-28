69b989087b5ef352e1feb89170527cdd0b639f28
AshishProPec <ashishpersonal200@gmail.com>
2026-07-28 08:29:48 +0530

    update

--- math/geometry/149MaxPointsOnLine.cpp
+++ math/geometry/149MaxPointsOnLine.cpp
@@ -135,3 +135,88 @@ public:
         return ans;
     }
 };
+
+
+// best approaches
+
+// Approach-2 (Using map - O(n^2) and atan2 C++ STL)
+class Solution
+{
+public:
+    int maxPoints(vector<vector<int>> &points)
+    {
+        int n = points.size();
+        if (n == 1)
+            return 1;
+
+        int result = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+
+            unordered_map<double, int> mp;
+
+            for (int j = 0; j < n; j++)
+            {
+
+                if (i == j)
+                    continue;
+
+                auto dy = points[j][1] - points[i][1];
+                auto dx = points[j][0] - points[i][0];
+                auto slope = atan2(dy, dx);
+
+                mp[slope]++;
+            }
+
+            for (auto &it : mp)
+            {
+                result = max(result, it.second + 1);
+            }
+        }
+
+        return result;
+    }
+};
+
+// Approach-3 (Without using atan2)
+class Solution
+{
+public:
+    int maxPoints(vector<vector<int>> &points)
+    {
+        int n = points.size();
+        if (n == 1)
+            return 1;
+
+        int result = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+
+            unordered_map<string, int> mp;
+
+            for (int j = 0; j < n; j++)
+            {
+
+                if (i == j)
+                    continue;
+
+                auto dy = points[j][1] - points[i][1];
+                auto dx = points[j][0] - points[i][0];
+                auto gcd = __gcd(dy, dx);
+
+                string key = to_string(dy / gcd) + "_" + to_string(dx / gcd);
+
+                mp[key]++;
+            }
+
+            for (auto &it : mp)
+            {
+                result = max(result, it.second + 1);
+            }
+        }
+
+        return result;
+    }
+};
