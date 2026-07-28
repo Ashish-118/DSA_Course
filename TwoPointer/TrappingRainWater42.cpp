3cc904032ad51aa9252bcbac8c3b1186aa33ce9c
AshishProPec <ashishpersonal200@gmail.com>
2026-07-28 16:09:10 +0530

    update

--- TwoPointer/TrappingRainWater42.cpp
+++ TwoPointer/TrappingRainWater42.cpp
@@ -28,3 +28,48 @@ public:
         return water;
     }
 };
+
+// another way
+
+class Solution
+{
+public:
+    int trap(vector<int> &height)
+    {
+        int maxi = *max_element(begin(height), end(height));
+        int n = height.size();
+        long long totalArea = maxi * n;
+
+        int prev = height[0];
+        int curr = -1;
+        totalArea -= height[0];
+
+        for (int i = 1; i < n; i++)
+        {
+            curr = height[i];
+            totalArea -= height[i];
+
+            if (prev < curr)
+            {
+                totalArea -= (1LL * i * (curr - prev));
+                prev = curr;
+            }
+        }
+
+        curr = -1;
+        prev = height[n - 1];
+
+        for (int i = n - 2; i >= 0; i--)
+        {
+            curr = height[i];
+
+            if (prev < curr)
+            {
+                totalArea -= (1LL * (n - 1 - i) * (curr - prev));
+                prev = curr;
+            }
+        }
+
+        return totalArea;
+    }
+};
