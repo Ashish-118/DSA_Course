e98d1028ed5a5ffab3a9834cbcdff3dc908e321b
AshishProPec <ashishpersonal200@gmail.com>
2026-07-29 22:25:24 +0530

    update

--- leetDaily/smallestPalindromicRearrangementII.cpp
+++ leetDaily/smallestPalindromicRearrangementII.cpp
@@ -1,12 +1,16 @@
-class Solution {
+class Solution
+{
 private:
-    long long comb(long long n, long long m, long long k) {
+    long long comb(long long n, long long m, long long k)
+    {
         long long res = 1;
         m = std::min(m, n - m);
 
-        for (long long i = 1; i <= m; i++) {
+        for (long long i = 1; i <= m; i++)
+        {
             res = res * (n - i + 1) / i;
-            if (res > k) {
+            if (res > k)
+            {
                 return k + 1;
             }
         }
@@ -14,23 +18,29 @@ private:
     }
 
 public:
-    std::string smallestPalindrome(std::string s, long long k) {
+    std::string smallestPalindrome(std::string s, long long k)
+    {
         int partition = s.length() / 2;
         std::vector<int> bucket(26, 0);
 
-        for (int i = 0; i < partition; i++) {
+        for (int i = 0; i < partition; i++)
+        {
             bucket[s[i] - 'a'] += 1;
         }
 
-        auto permutations = [&](int rem) {
+        auto permutations = [&](int rem)
+        {
             long long ways = 1;
-            for (int i = 0; i < 26; i++) {
-                if (bucket[i] == 0) {
+            for (int i = 0; i < 26; i++)
+            {
+                if (bucket[i] == 0)
+                {
                     continue;
                 }
 
                 ways *= comb(rem, bucket[i], k);
-                if (ways > k) {
+                if (ways > k)
+                {
                     break;
                 }
                 rem -= bucket[i];
@@ -41,16 +51,20 @@ public:
         std::string left = "";
         long long startIndex = 1;
 
-        for (int pos = 0; pos < partition; pos++) {
-            for (int i = 0; i < 26; i++) {
-                if (bucket[i] == 0) {
+        for (int pos = 0; pos < partition; pos++)
+        {
+            for (int i = 0; i < 26; i++)
+            {
+                if (bucket[i] == 0)
+                {
                     continue;
                 }
 
                 bucket[i] -= 1;
 
                 long long ways = permutations(partition - pos - 1);
-                if (startIndex + ways > k) {
+                if (startIndex + ways > k)
+                {
                     left += (char)(i + 'a');
                     break;
                 }
@@ -60,7 +74,8 @@ public:
             }
         }
 
-        if (left.length() < partition) {
+        if (left.length() < partition)
+        {
             return "";
         }
 
