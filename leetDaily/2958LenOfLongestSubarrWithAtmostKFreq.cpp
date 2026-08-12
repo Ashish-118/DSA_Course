5d2571fc2c61b014571baf02b00b37eda1db5acd
AshishProPec <ashishpersonal200@gmail.com>
2026-08-12 22:39:52 +0530

    potd

---
+++ leetDaily/2958LenOfLongestSubarrWithAtmostKFreq.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    int maxSubarrayLength(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+
+        int left = 0;
+        unordered_map<int, int> freq;
+
+        int ans = 0;
+
+        for (int right = 0; right < n; right++)
+        {
+            freq[nums[right]]++;
+
+            while (freq[nums[right]] > k)
+            {
+                freq[nums[left]]--;
+                left++;
+            }
+            ans = max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+};
