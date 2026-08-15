217c41ff3b691c06ed451356bb63439054d5d2b9
AshishProPec <ashishpersonal200@gmail.com>
2026-08-15 10:29:05 +0530

    update

--- greedy/cf_offShores.cpp
+++ greedy/cf_offShores.cpp
@@ -12,71 +12,20 @@ int main()
         cin >> n >> x >> y;
 
         vector<int> arr(n);
+        long long total_transfer_sum=0;
         for (int i = 0; i < n; i++)
         {
             cin >> arr[i];
+            total_transfer_sum+=(arr[i]/x)*y;
         }
 
-        vector<int> rem(n);
-        for (int i = 0; i < n; i++)
-        {
-            rem[i] = arr[i] % x;
+        long long ans=0;
+
+        for(int i=0; i<n; i++){
+            ans=max(ans, total_transfer_sum-((arr[i]/x)*y)+arr[i]);
         }
 
-        queue<int> q;
-
-        for (int i = 0; i < n; i++)
-        {
-            if (rem[i] == 0)
-                q.push(i);
-        }
-
-        while (!q.empty())
-        {
-            int curr = q.front();
-            q.pop();
-
-            for (int i = 0; i < n && arr[curr]; i++)
-            {
-
-                if (rem[i] == 0)
-                    continue;
-
-                int times = arr[curr] / x;
-                for (int j = 1; j <= times; j++)
-                {
-                    if ((rem[i] + (j * y)) % x == 0)
-                    {
-                        arr[i] += j * y;
-                        rem[i] = 0;
-                        q.push(i);
-                        arr[curr] -= j * x;
-                        break;
-                    }
-                }
-            }
-        }
-
-        int idx = max_element(rem.begin(), rem.end()) - rem.begin();
-        if (rem[idx] == 0)
-        {
-            int maxiElementIdx = max_element(arr.begin(), arr.end()) - arr.begin();
-            idx = maxiElementIdx;
-        }
-
-        // for(int i=0; i<n; i++){
-        //     cout<<"i "<<i<<" rem "<<rem[i]<<" val "<<arr[i]<<endl;
-        // }
-
-        int ans = arr[idx];
-
-        for (int i = 0; i < n; i++)
-        {
-            if (i != idx)
-            {
-                ans += ((arr[i] / x) * y);
-            }
-        }
+       
 
         cout << ans << endl;
     }
