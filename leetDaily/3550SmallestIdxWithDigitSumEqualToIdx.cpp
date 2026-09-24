0efeaa0ae80189c117d8a64e9d42745146d65c42
AshishProPec <ashishpersonal200@gmail.com>
2026-09-24 17:44:07 +0530

    potd

---
+++ leetDaily/3550SmallestIdxWithDigitSumEqualToIdx.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    int smallestIndex(vector<int> &nums)
+    {
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            string num = to_string(nums[i]);
+            int sum = 0;
+            for (auto d : num)
+            {
+                sum += d - '0';
+            }
+
+            if (sum == i)
+                return i;
+        }
+
+        return -1;
+    }
+};
